05fb6fb493e885918e7e52dbcf093a75a13dd5d9
pankaj <pankaj.kushwaha1990@sansad.nic.in>
2026-08-26 17:24:17 +0530

    Build frontend image in CI

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+// app/page.js
+export default function Home() {
+  return (
+    <main style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontFamily: 'sans-serif' 
+    }}>
+      <h1>this is my first deveOps journey</h1>
+    </main>
+  );
+}
